46d044808ea156865bf7ab29ff3f204d85cc1cfa
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2010-12-13 12:31:29 +0100

    FWRAP Ported changes of statlib.pyf

--- scipy/stats/statlib.pxd
+++ scipy/stats/statlib.pxd
@@ -1,7 +1,7 @@
 cimport numpy as np
 from statlib_fc cimport *
 
-cpdef api object wprob(fwi_integer_t test, fwi_integer_t other, fwr_real_t astart, object a1, fwi_integer_t l1, object a2, object a3, fwi_integer_t ifault)
-cpdef api object gscale(fwi_integer_t test, fwi_integer_t other, fwr_real_t astart, object a1, fwi_integer_t l1, object a2, object a3, fwi_integer_t ifault)
-cpdef api object prho(fwi_integer_t n, fwi_integer_t is__, fwi_integer_t ifault)
-cpdef api object swilk(bint init, object x, fwi_integer_t n, fwi_integer_t n1, fwi_integer_t n2, object a, fwr_real_t w, fwr_real_t pw, fwi_integer_t ifault)
+cpdef api object wprob(fwi_integer_t test, fwi_integer_t other, object a1=*)
+cpdef api object gscale(fwi_integer_t test, fwi_integer_t other, object a1=*)
+cpdef api object prho(fwi_integer_t n, fwi_integer_t is__)
+cpdef api object swilk(object x, object a, bint init=*, object n1=*)
--- scipy/stats/statlib.pyx
+++ scipy/stats/statlib.pyx
@@ -27,172 +27,159 @@ np.import_array()
 include 'fwrap_ktp.pxi'
 cdef extern from "string.h":
     void *memcpy(void *dest, void *src, size_t n)
-cpdef api object wprob(fwi_integer_t test, fwi_integer_t other, fwr_real_t astart, object a1, fwi_integer_t l1, object a2, object a3, fwi_integer_t ifault):
+cpdef api object wprob(fwi_integer_t test, fwi_integer_t other, object a1=None):
     """
-    wprob(test, other, astart, a1, l1, a2, a3, ifault) -> (test, other, astart, a1, l1, a2, a3, ifault)
+    wprob(test, other, [a1]) -> (astart, a1, ifault)
 
     Parameters
     ----------
-    test : fwi_integer, intent inout
-    other : fwi_integer, intent inout
-    astart : fwr_real, intent inout
-    a1 : fwr_real, 1D array, dimension(l1), intent inout
-    l1 : fwi_integer, intent inout
-    a2 : fwr_real, 1D array, dimension(l1), intent inout
-    a3 : fwr_real, 1D array, dimension(l1), intent inout
-    ifault : fwi_integer, intent inout
+    test : fwi_integer, intent in
+    other : fwi_integer, intent in
+    a1 : fwr_real, 1D array, dimension(l1), intent out
 
     Returns
     -------
-    test : fwi_integer, intent inout
-    other : fwi_integer, intent inout
-    astart : fwr_real, intent inout
-    a1 : fwr_real, 1D array, dimension(l1), intent inout
-    l1 : fwi_integer, intent inout
-    a2 : fwr_real, 1D array, dimension(l1), intent inout
-    a3 : fwr_real, 1D array, dimension(l1), intent inout
-    ifault : fwi_integer, intent inout
+    astart : fwr_real, intent out
+    a1 : fwr_real, 1D array, dimension(l1), intent out
+    ifault : fwi_integer, intent out
 
     """
     cdef np.ndarray a1_
     cdef fw_shape_t a1_shape_[1]
+    cdef fwi_integer_t l1
     cdef np.ndarray a2_
     cdef fw_shape_t a2_shape_[1]
     cdef np.ndarray a3_
     cdef fw_shape_t a3_shape_[1]
     cdef fwi_integer_t fw_iserr__
+    cdef fwr_real_t astart
+    cdef fwi_integer_t ifault
     cdef fw_character_t fw_errstr__[fw_errstr_len]
-    a1_, a1 = fw_asfortranarray(a1, fwr_real_t_enum, 1, False)
+    l1 = 1 + ((test * other) // 2)
+    a1_, a1 = fw_explicitshapearray(a1, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a1_shape_, np.PyArray_DIMS(a1_), 1)
-    a2_, a2 = fw_asfortranarray(a2, fwr_real_t_enum, 1, False)
+    a2_, a2 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a2_shape_, np.PyArray_DIMS(a2_), 1)
-    a3_, a3 = fw_asfortranarray(a3, fwr_real_t_enum, 1, False)
+    a3_, a3 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a3_shape_, np.PyArray_DIMS(a3_), 1)
     wprob_c(&test, &other, &astart, a1_shape_, <fwr_real_t*>np.PyArray_DATA(a1_), &l1, a2_shape_, <fwr_real_t*>np.PyArray_DATA(a2_), a3_shape_, <fwr_real_t*>np.PyArray_DATA(a3_), &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'wprob' wrapper.")
-    return (test, other, astart, a1, l1, a2, a3, ifault,)
+    return (astart, a1, ifault,)
 
 
-cpdef api object gscale(fwi_integer_t test, fwi_integer_t other, fwr_real_t astart, object a1, fwi_integer_t l1, object a2, object a3, fwi_integer_t ifault):
+cpdef api object gscale(fwi_integer_t test, fwi_integer_t other, object a1=None):
     """
-    gscale(test, other, astart, a1, l1, a2, a3, ifault) -> (test, other, astart, a1, l1, a2, a3, ifault)
+    gscale(test, other, [a1]) -> (astart, a1, ifault)
 
     Parameters
     ----------
-    test : fwi_integer, intent inout
-    other : fwi_integer, intent inout
-    astart : fwr_real, intent inout
-    a1 : fwr_real, 1D array, dimension(l1), intent inout
-    l1 : fwi_integer, intent inout
-    a2 : fwr_real, 1D array, dimension(l1), intent inout
-    a3 : fwr_real, 1D array, dimension(l1), intent inout
-    ifault : fwi_integer, intent inout
+    test : fwi_integer, intent in
+    other : fwi_integer, intent in
+    a1 : fwr_real, 1D array, dimension(l1), intent out
 
     Returns
     -------
-    test : fwi_integer, intent inout
-    other : fwi_integer, intent inout
-    astart : fwr_real, intent inout
-    a1 : fwr_real, 1D array, dimension(l1), intent inout
-    l1 : fwi_integer, intent inout
-    a2 : fwr_real, 1D array, dimension(l1), intent inout
-    a3 : fwr_real, 1D array, dimension(l1), intent inout
-    ifault : fwi_integer, intent inout
+    astart : fwr_real, intent out
+    a1 : fwr_real, 1D array, dimension(l1), intent out
+    ifault : fwi_integer, intent out
 
     """
     cdef np.ndarray a1_
     cdef fw_shape_t a1_shape_[1]
+    cdef fwi_integer_t l1
     cdef np.ndarray a2_
     cdef fw_shape_t a2_shape_[1]
     cdef np.ndarray a3_
     cdef fw_shape_t a3_shape_[1]
     cdef fwi_integer_t fw_iserr__
+    cdef fwr_real_t astart
+    cdef fwi_integer_t ifault
     cdef fw_character_t fw_errstr__[fw_errstr_len]
-    a1_, a1 = fw_asfortranarray(a1, fwr_real_t_enum, 1, False)
+    l1 = 1 + ((test * other) // 2)
+    a1_, a1 = fw_explicitshapearray(a1, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a1_shape_, np.PyArray_DIMS(a1_), 1)
-    a2_, a2 = fw_asfortranarray(a2, fwr_real_t_enum, 1, False)
+    a2_, a2 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a2_shape_, np.PyArray_DIMS(a2_), 1)
-    a3_, a3 = fw_asfortranarray(a3, fwr_real_t_enum, 1, False)
+    a3_, a3 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a3_shape_, np.PyArray_DIMS(a3_), 1)
     gscale_c(&test, &other, &astart, a1_shape_, <fwr_real_t*>np.PyArray_DATA(a1_), &l1, a2_shape_, <fwr_real_t*>np.PyArray_DATA(a2_), a3_shape_, <fwr_real_t*>np.PyArray_DATA(a3_), &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'gscale' wrapper.")
-    return (test, other, astart, a1, l1, a2, a3, ifault,)
+    return (astart, a1, ifault,)
 
 
-cpdef api object prho(fwi_integer_t n, fwi_integer_t is__, fwi_integer_t ifault):
+cpdef api object prho(fwi_integer_t n, fwi_integer_t is__):
     """
-    prho(n, is__, ifault) -> (fw_ret_arg, n, is__, ifault)
+    prho(n, is__) -> (fw_ret_arg, ifault)
 
     Parameters
     ----------
-    n : fwi_integer, intent inout
-    is__ : fwi_integer, intent inout
-    ifault : fwi_integer, intent inout
+    n : fwi_integer, intent in
+    is__ : fwi_integer, intent in
 
     Returns
     -------
     fw_ret_arg : fwr_dbl, intent out
-    n : fwi_integer, intent inout
-    is__ : fwi_integer, intent inout
-    ifault : fwi_integer, intent inout
+    ifault : fwi_integer, intent out
 
     """
     cdef fwi_integer_t fw_iserr__
     cdef fwr_dbl_t fw_ret_arg
+    cdef fwi_integer_t ifault
     cdef fw_character_t fw_errstr__[fw_errstr_len]
     prho_c(&fw_ret_arg, &n, &is__, &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'prho' wrapper.")
-    return (fw_ret_arg, n, is__, ifault,)
+    return (fw_ret_arg, ifault,)
 
 
-cpdef api object swilk(bint init, object x, fwi_integer_t n, fwi_integer_t n1, fwi_integer_t n2, object a, fwr_real_t w, fwr_real_t pw, fwi_integer_t ifault):
+cpdef api object swilk(object x, object a, bint init=0, object n1=None):
     """
-    swilk(init, x, n, n1, n2, a, w, pw, ifault) -> (init_, x, n, n1, n2, a, w, pw, ifault)
+    swilk(x, a, init, n1) -> (a, w, pw, ifault)
 
     Parameters
     ----------
-    init : fwl_logical, intent inout
-    x : fwr_real, 1D array, dimension(*), intent inout
-    n : fwi_integer, intent inout
-    n1 : fwi_integer, intent inout
-    n2 : fwi_integer, intent inout
-    a : fwr_real, 1D array, dimension(*), intent inout
-    w : fwr_real, intent inout
-    pw : fwr_real, intent inout
-    ifault : fwi_integer, intent inout
+    x : fwr_real, 1D array, dimension(n), intent in
+    a : fwr_real, 1D array, dimension(n2), intent inout
+    init : fwl_logical, intent in
+    n1 : fwi_integer, intent in
 
     Returns
     -------
-    init : fwl_logical, intent inout
-    x : fwr_real, 1D array, dimension(*), intent inout
-    n : fwi_integer, intent inout
-    n1 : fwi_integer, intent inout
-    n2 : fwi_integer, intent inout
-    a : fwr_real, 1D array, dimension(*), intent inout
-    w : fwr_real, intent inout
-    pw : fwr_real, intent inout
-    ifault : fwi_integer, intent inout
+    a : fwr_real, 1D array, dimension(n2), intent inout
+    w : fwr_real, intent out
+    pw : fwr_real, intent out
+    ifault : fwi_integer, intent out
 
     """
-    cdef fwl_logical_t init_
     cdef np.ndarray x_
     cdef fw_shape_t x_shape_[1]
     cdef np.ndarray a_
     cdef fw_shape_t a_shape_[1]
+    cdef fwl_logical_t init_
+    cdef fwi_integer_t n1_
+    cdef fwi_integer_t n
+    cdef fwi_integer_t n2
     cdef fwi_integer_t fw_iserr__
+    cdef fwr_real_t w
+    cdef fwr_real_t pw
+    cdef fwi_integer_t ifault
     cdef fw_character_t fw_errstr__[fw_errstr_len]
+    a_, a = fw_asfortranarray(a, fwr_real_t_enum, 1, False)
+    fw_copyshape(a_shape_, np.PyArray_DIMS(a_), 1)
     init_ = 1 if init else 0
     x_, x = fw_asfortranarray(x, fwr_real_t_enum, 1, False)
     fw_copyshape(x_shape_, np.PyArray_DIMS(x_), 1)
-    a_, a = fw_asfortranarray(a, fwr_real_t_enum, 1, False)
-    fw_copyshape(a_shape_, np.PyArray_DIMS(a_), 1)
-    swilk_c(&init_, x_shape_, <fwr_real_t*>np.PyArray_DATA(x_), &n, &n1, &n2, a_shape_, <fwr_real_t*>np.PyArray_DATA(a_), &w, &pw, &ifault, &fw_iserr__, fw_errstr__)
+    n = np.PyArray_DIMS(x_)[0]
+    n2 = n // 2
+    n1_ = n1 if (n1 is not None) else n
+    if not (n1_ <= n):
+        raise ValueError('Condition on arguments not satisfied: n1 <= n')
+    swilk_c(&init_, x_shape_, <fwr_real_t*>np.PyArray_DATA(x_), &n, &n1_, &n2, a_shape_, <fwr_real_t*>np.PyArray_DATA(a_), &w, &pw, &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'swilk' wrapper.")
-    return (init_, x, n, n1, n2, a, w, pw, ifault,)
+    return (a, w, pw, ifault,)
 
 
 
@@ -256,6 +243,14 @@ cdef object fw_f2py_shape_coercion(int to_ndim, object to_shape,
                 to_shape[i] = d
             else:
                 to_shape[to_ndim - 1] *= d    
+
+cdef object fw_explicitshapearray(object value, int typenum, int ndim,
+                                  np.intp_t *shape, bint copy):
+    if value is None:
+        result = np.PyArray_ZEROS(ndim, shape, typenum, 1)
+        return result, result
+    else:
+        return fw_asfortranarray(value, typenum, ndim, copy)
 
 # Fwrap configuration:
 # Fwrap: version 0.2.0dev_b9c96b3
