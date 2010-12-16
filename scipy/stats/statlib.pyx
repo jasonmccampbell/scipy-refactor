b07258af6823f971ab87238d1d8d3bcff2ee4803
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2010-12-16 12:41:25 +0100

    FWRAP Ported changes of statlib.pyf

--- scipy/stats/statlib.pyx
+++ scipy/stats/statlib.pyx
@@ -1,7 +1,7 @@
 #cython: ccomplex=True
 
 """
-The statlib module was generated with Fwrap v0.2.0dev_b9c96b3.
+The statlib module was generated with Fwrap v0.2.0dev_470490d.
 
 Below is a listing of functions and data types.
 For usage information see the function docstrings.
@@ -58,10 +58,16 @@ cpdef api object wprob(fwi_integer_t test, fwi_integer_t other, object a1=None):
     l1 = 1 + ((test * other) // 2)
     a1_, a1 = fw_explicitshapearray(a1, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a1_shape_, np.PyArray_DIMS(a1_), 1)
+    if l1 != a1_shape_[0]:
+        raise ValueError("(l1 == a1.shape[0]) not satisifed")
     a2_, a2 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a2_shape_, np.PyArray_DIMS(a2_), 1)
+    if l1 != a2_shape_[0]:
+        raise ValueError("(l1 == a2.shape[0]) not satisifed")
     a3_, a3 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a3_shape_, np.PyArray_DIMS(a3_), 1)
+    if l1 != a3_shape_[0]:
+        raise ValueError("(l1 == a3.shape[0]) not satisifed")
     wprob_c(&test, &other, &astart, a1_shape_, <fwr_real_t*>np.PyArray_DATA(a1_), &l1, a2_shape_, <fwr_real_t*>np.PyArray_DATA(a2_), a3_shape_, <fwr_real_t*>np.PyArray_DATA(a3_), &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'wprob' wrapper.")
@@ -99,10 +105,16 @@ cpdef api object gscale(fwi_integer_t test, fwi_integer_t other, object a1=None)
     l1 = 1 + ((test * other) // 2)
     a1_, a1 = fw_explicitshapearray(a1, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a1_shape_, np.PyArray_DIMS(a1_), 1)
+    if not (0 <= l1 <= a1_shape_[0]):
+        raise ValueError("(0 <= l1 <= a1.shape[0]) not satisifed")
     a2_, a2 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a2_shape_, np.PyArray_DIMS(a2_), 1)
+    if l1 != a2_shape_[0]:
+        raise ValueError("(l1 == a2.shape[0]) not satisifed")
     a3_, a3 = fw_explicitshapearray(None, fwr_real_t_enum, 1, [l1], False)
     fw_copyshape(a3_shape_, np.PyArray_DIMS(a3_), 1)
+    if l1 != a3_shape_[0]:
+        raise ValueError("(l1 == a3.shape[0]) not satisifed")
     gscale_c(&test, &other, &astart, a1_shape_, <fwr_real_t*>np.PyArray_DATA(a1_), &l1, a2_shape_, <fwr_real_t*>np.PyArray_DATA(a2_), a3_shape_, <fwr_real_t*>np.PyArray_DATA(a3_), &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'gscale' wrapper.")
@@ -166,16 +178,20 @@ cpdef api object swilk(object x, object a, bint init=0, object n1=None):
     cdef fwr_real_t pw
     cdef fwi_integer_t ifault
     cdef fw_character_t fw_errstr__[fw_errstr_len]
-    a_, a = fw_asfortranarray(a, fwr_real_t_enum, 1, False)
-    fw_copyshape(a_shape_, np.PyArray_DIMS(a_), 1)
     init_ = 1 if init else 0
     x_, x = fw_asfortranarray(x, fwr_real_t_enum, 1, False)
     fw_copyshape(x_shape_, np.PyArray_DIMS(x_), 1)
     n = np.PyArray_DIMS(x_)[0]
-    n2 = n // 2
     n1_ = n1 if (n1 is not None) else n
     if not (n1_ <= n):
         raise ValueError('Condition on arguments not satisfied: n1 <= n')
+    if not (0 <= n <= x_shape_[0]):
+        raise ValueError("(0 <= n <= x.shape[0]) not satisifed")
+    a_, a = fw_asfortranarray(a, fwr_real_t_enum, 1, False)
+    fw_copyshape(a_shape_, np.PyArray_DIMS(a_), 1)
+    n2 = n // 2
+    if n2 != a_shape_[0]:
+        raise ValueError("(n2 == a.shape[0]) not satisifed")
     swilk_c(&init_, x_shape_, <fwr_real_t*>np.PyArray_DATA(x_), &n, &n1_, &n2, a_shape_, <fwr_real_t*>np.PyArray_DATA(a_), &w, &pw, &ifault, &fw_iserr__, fw_errstr__)
     if fw_iserr__ != FW_NO_ERR__:
         raise RuntimeError("an error was encountered when calling the 'swilk' wrapper.")
@@ -190,15 +206,22 @@ cdef void fw_copyshape(fw_shape_t *target, np.intp_t *source, int ndim):
     for i in range(ndim):
         target[i] = source[i]
 
+cdef object fw_explicitshapearray(object value, int typenum, int ndim,
+                                  np.intp_t *shape, bint copy):
+    if value is None:
+        result = np.PyArray_ZEROS(ndim, shape, typenum, 1)
+        return result, result
+    else:
+        return fw_asfortranarray(value, typenum, ndim, copy)
+
 cdef object fw_asfortranarray(object value, int typenum, int ndim, bint copy):
-    cdef int flags = np.NPY_F_CONTIGUOUS
+    cdef int flags = np.NPY_F_CONTIGUOUS | np.NPY_FORCECAST
     if ndim <= 1:
         # See http://projects.scipy.org/numpy/ticket/1691 for why this is needed
         flags |= np.NPY_C_CONTIGUOUS
     if copy:
         flags |= np.NPY_ENSURECOPY
     result = np.PyArray_FROMANY(value, typenum, 0, 0, flags)
-
 
     if ndim == result.ndim:
         return result, result
@@ -242,22 +265,21 @@ cdef object fw_f2py_shape_coercion(int to_ndim, object to_shape,
             if i < to_ndim:
                 to_shape[i] = d
             else:
-                to_shape[to_ndim - 1] *= d    
-
-cdef object fw_explicitshapearray(object value, int typenum, int ndim,
-                                  np.intp_t *shape, bint copy):
-    if value is None:
-        result = np.PyArray_ZEROS(ndim, shape, typenum, 1)
-        return result, result
-    else:
-        return fw_asfortranarray(value, typenum, ndim, copy)
+                to_shape[to_ndim - 1] *= d
 
 # Fwrap configuration:
-# Fwrap: version 0.2.0dev_b9c96b3
+# Fwrap: version 0.2.0dev_470490d
 # Fwrap: self-sha1 0ff006c1ae5e8d77511a99502334d391ba122ad8
-# Fwrap: pyf-sha1 46d044808ea156865bf7ab29ff3f204d85cc1cfa
+# Fwrap: pyf-sha1 d8bc63084810f03728b2682b1c32903e988002ac
 # Fwrap: wraps statlib/*.f
 # Fwrap:     sha1 fdacc5bbd4afbc5c3867c04dad74d9ea6d64b3bd
+# Fwrap: exclude alnorm
+# Fwrap: exclude frqadd
+# Fwrap: exclude imply
+# Fwrap: exclude poly
+# Fwrap: exclude ppnd
+# Fwrap: exclude start1
+# Fwrap: exclude start2
 # Fwrap: exclude alnorm
 # Fwrap: exclude frqadd
 # Fwrap: exclude imply
