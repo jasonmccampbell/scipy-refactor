9cb4b4f9a5f91f056ce52159b86c6f38127dbbef
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2011-01-18 13:43:37 +0100

    FWRAP Mangle 'is' argument name

--- scipy/stats/statlib_fc.h
+++ scipy/stats/statlib_fc.h
@@ -48,7 +48,7 @@ extern "C" {
 #endif
 FORTRAN_CALLSPEC void F_FUNC(wprob,WPROB)(fwi_integer_t * test, fwi_integer_t * other, fwr_real_t * astart, fwr_real_t * a1, fwi_integer_t * l1, fwr_real_t * a2, fwr_real_t * a3, fwi_integer_t * ifault);
 FORTRAN_CALLSPEC void F_FUNC(gscale,GSCALE)(fwi_integer_t * test, fwi_integer_t * other, fwr_real_t * astart, fwr_real_t * a1, fwi_integer_t * l1, fwr_real_t * a2, fwr_real_t * a3, fwi_integer_t * ifault);
-FORTRAN_CALLSPEC fwr_dbl_t F_FUNC(prho,PRHO)(fwi_integer_t * n, fwi_integer_t * is, fwi_integer_t * ifault);
+FORTRAN_CALLSPEC fwr_dbl_t F_FUNC(prho,PRHO)(fwi_integer_t * n, fwi_integer_t * is_, fwi_integer_t * ifault);
 FORTRAN_CALLSPEC void F_FUNC(swilk,SWILK)(fwl_logical_t * init, fwr_real_t * x, fwi_integer_t * n, fwi_integer_t * n1, fwi_integer_t * n2, fwr_real_t * a, fwr_real_t * w, fwr_real_t * pw, fwi_integer_t * ifault);
 #if defined(__cplusplus)
 } /* extern "C" */
--- scipy/stats/statlib_fc.pxd
+++ scipy/stats/statlib_fc.pxd
@@ -3,5 +3,5 @@ from fwrap_ktp cimport *
 cdef extern from "statlib_fc.h":
     void wprob(fwi_integer_t * test, fwi_integer_t * other, fwr_real_t * astart, fwr_real_t * a1, fwi_integer_t * l1, fwr_real_t * a2, fwr_real_t * a3, fwi_integer_t * ifault)
     void gscale(fwi_integer_t * test, fwi_integer_t * other, fwr_real_t * astart, fwr_real_t * a1, fwi_integer_t * l1, fwr_real_t * a2, fwr_real_t * a3, fwi_integer_t * ifault)
-    fwr_dbl_t prho(fwi_integer_t * n, fwi_integer_t * is, fwi_integer_t * ifault)
+    fwr_dbl_t prho(fwi_integer_t * n, fwi_integer_t * is_, fwi_integer_t * ifault)
     void swilk(fwl_logical_t * init, fwr_real_t * x, fwi_integer_t * n, fwi_integer_t * n1, fwi_integer_t * n2, fwr_real_t * a, fwr_real_t * w, fwr_real_t * pw, fwi_integer_t * ifault)
