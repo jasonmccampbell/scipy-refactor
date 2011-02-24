6bcd8515959d71a8a29b5e57c1fbc049d2664af0
Jason McCampbell (Enthought, Inc) <jmccampbell@enthought.com>
2011-02-24 17:37:23 -0600

    Fixed build of cluster, still need to wrap hierarchy.

--- scipy/cluster/src/vq.h
+++ scipy/cluster/src/vq.h
@@ -1,9 +1,7 @@
 #ifndef _VQ_H_
 #define _VQ_H
 
-#include <Python.h>
-
-#include <numpy/arrayobject.h>
+#include <npy_api.h>
 
 int double_tvq(double* obs, double* code_book, int Nobs, int Ncodes, 
         int Nfeatures, npy_intp* codes, double* lowest_dist);
