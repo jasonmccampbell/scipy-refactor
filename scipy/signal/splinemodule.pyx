86d4b03418f488c9fdbaab31c2391e64eb9ce1ef
Ilan Schnell <ischnell@enthought.com>
2011-02-25 15:22:03 -0600

    cleanup

--- scipy/signal/splinemodule.pyx
+++ scipy/signal/splinemodule.pyx
@@ -15,35 +15,29 @@ static void convert_strides(npy_intp*,npy_intp*,int,int);
 
 def extern from "S_bspline_util.h":
     int S_cubic_spline2D(float*, float*, int, int, double, npy_intp*,
-                         npy_intp*,float);
+                         npy_intp*,float)
     int S_quadratic_spline2D(float*, float*, int, int, double, npy_intp*,
-                             npy_intp*, float);
-    int S_IIR_forback1(float, float, float*, float*, int, int, int, float);
-    int S_IIR_forback2(double, double, float*, float*, int, int, int, float);
+                             npy_intp*, float)
+    int S_IIR_forback1(float, float, float*, float*, int, int, int, float)
+    int S_IIR_forback2(double, double, float*, float*, int, int, int, float)
     int S_separable_2Dconvolve_mirror(float*, float*, int, int, float*, float*,
-                                      int, int, npy_intp*, npy_intp*);
+                                      int, int, npy_intp*, npy_intp*)
 
 def extern from "D_bspline_util.h":
     int D_cubic_spline2D(double*, double*, int, int, double, npy_intp*,
-                         npy_intp*, double);
-    int D_quadratic_spline2D(double*,double*,int,int,double,npy_intp*,
-                             npy_intp*,double);
-    int D_IIR_forback1(double,double,double*,double*,int,int,int,double);
-    int D_IIR_forback2(double,double,double*,double*,int,int,int,double); 
-    int D_separable_2Dconvolve_mirror(double*,double*,int,int,double*,double*,
-                                      int,int,npy_intp*,npy_intp*);
+                         npy_intp*, double)
+    int D_quadratic_spline2D(double*, double*, int, int, double, npy_intp*,
+                             npy_intp*, double)
+    int D_IIR_forback1(double, double, double*, double*, int, int, int, double)
+    int D_IIR_forback2(double, double, double*, double*, int, int, int, double) 
+    int D_separable_2Dconvolve_mirror(double*, double*, int, int, double*,
+                                      double*, int, int, npy_intp*, npy_intp*)
 
-#ifdef __GNUC__
-extern int C_IIR_forback1(__complex__ float,__complex__ float,__complex__ float*,__complex__ float*,int,int,int,float);
-extern int C_separable_2Dconvolve_mirror(__complex__ float*,__complex__ float*,int,int,__complex__ float*,__complex__ float*,int,int,npy_intp*,npy_intp*);
-extern int Z_IIR_forback1(__complex__ double,__complex__ double,__complex__ double*,__complex__ double*,int,int,int,double);
-extern int Z_separable_2Dconvolve_mirror(__complex__ double*,__complex__ double*,int,int,__complex__ double*,__complex__ double*,int,int,npy_intp*,npy_intp*);
-#endif
 
-static void
-convert_strides(npy_intp* instrides,npy_intp* convstrides,int size,int N)
-{
-  int n; npy_intp bitshift;
+cdef void convert_strides(npy_intp* instrides,
+                          npy_intp* convstrides,
+                          int size, int N):
+int n; npy_intp bitshift;
 
   bitshift = -1;
 
