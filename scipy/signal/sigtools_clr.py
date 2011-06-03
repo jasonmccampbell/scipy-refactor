70da979b428f5495786a431a63447a0b2d329b85
Jason McCampbell (Enthought, Inc) <jmccampbell@enthought.com>
2011-06-03 13:51:12 -0500

    Fixed import of _remez from sigtoolsmod.

--- scipy/signal/sigtools_clr.py
+++ scipy/signal/sigtools_clr.py
@@ -5,7 +5,7 @@ if sys.platform == 'cli':
     clr.AddReference("signal")
 
     from scipy__signal__sigtoolsmod import _linear_filter
-    from scipy__signal__sigtoolsmod import _order_filterND
+    from scipy__signal__sigtoolsmod import _order_filterND, _remez
     from scipy__signal__sigtoolsmod import _correlateND, _medfilt2d
     from scipy__signal__sigtoolsmod import *
 
